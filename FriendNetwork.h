8cbfd2348d06cd2b6d725cd65c0f99a5daff4341
Kevin Lau <kevinlau@umail.ucsb.edu>
2015-11-18 14:40:01 -0800

    initial friend network

---
+++ FriendNetwork.h
@@ -0,0 +1,39 @@
+#ifndef FRIENDNETWORK_H
+#define FRIENDNETWORK_H
+#include <string>
+
+
+using namespace std;
+
+class FriendNetwork{
+    public:
+        int addFriend(User, User);
+        int deleteFriend(User, User);
+        int acceptRequest(User, User);
+        int rejectRequest(User, User);
+        string pendingRequests(User);
+        string friendList(User);
+        int postToFriend(User, User, string);
+        string viewFriendPosts(User);
+        string searchFriend(string);
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // FRIENDNETWORK_H
