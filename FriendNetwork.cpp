d48662636123ee27d94b119e722b998741c30c99
Kevin Lau <kevinlau@umail.ucsb.edu>
2015-11-18 15:00:57 -0800

    initial friendnetwork

--- FriendNetwork.cpp
+++ FriendNetwork.cpp
@@ -1,1 +1,17 @@
-  
+#include "FriendNetwork.h"
+#include "user.h"
+
+
+using namespace std;
+
+
+
+int FriendNetwork::addFriend(User self, User other){
+
+
+}
+
+int FriendNetwork::deleteFriend(User self, User other){
+
+
+}
